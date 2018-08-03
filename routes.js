2b0427de9fa872d3295af19662f41ab32c8eff94
cmccormack <mccormack.christopher@gmail.com>
2018-08-03 14:36:28 -0700

    Added some debug routes, new-user route, error handler

--- routes.js
+++ routes.js
@@ -1,10 +1,16 @@
 const path = require('path')
+const User = require('./userModel')
+
 const root = path.resolve(__dirname, ".")
 const publicPath = path.join(root, "public")
-const userModel = require('./userModel')
-const app = require('express')()
 
 module.exports = function(app) {
+
+  // DEBUG use to delete users as needed, delete later
+  // User.deleteOne({ _id: "5b64c412b8bef220dc4b4c0d"}, (err, user) => {
+  //   if (err) return next(new Error(err))
+  //   console.log(user)
+  // })
 
   ///////////////////////////////////////////////////////////
   // Testing/Debug Middleware
@@ -24,7 +30,7 @@ module.exports = function(app) {
 
   //this works for creating a user - can be deleted
   app.get('/test', (req, res, next) => {
-    const newUser = new userModel({
+    const newUser = new User({
       username: "me"
     })
 
@@ -38,16 +44,44 @@ module.exports = function(app) {
   });
 
   /*route for creating user
-  .post - req, res =>
+  .post /api/new-user - req, res =>
     check if user exists in db
     -if so respond with error or something
     -if not respond with success
 
   */
 
+  // Debug route to view users in DB
+  app.get("/api/users", (req, res, next) => {
+    User.find({})
+    .exec((err, users) => {
+      if (err) next(new Error(err))
+      res.json(users)
+    })
+  })
+
+  app.post('/api/new-user', (req, res, next) => {
+    const { username } = req.body
+    const newUser = new User({ username })
+    // Need to add username validation
+
+    newUser.save(err => {
+      if (err) {
+        if (err.name === 'MongoError' && err.code === 11000) {
+          return next(new Error(`Username '${username}' already taken`))
+        }
+        return next(new Error("Unable to process new user request"))
+      }
+      res.json({
+        success: true,
+        message: `User ${username} successfully added`
+      })
+    })
+  })
+
 
   /*route for submitting exercise
-    .post - req, res =>
+    .post /api/add-exercise - req, res =>
     Check if id or username/password match
     Add exercise to db => send success message?
     Error response if not
@@ -63,5 +97,18 @@ module.exports = function(app) {
   respond with json info if found
 
   */
+
+
+  ///////////////////////////////////////////////////////////
+  // Error Handler
+  ///////////////////////////////////////////////////////////
+  /* eslint no-unused-vars: 0 */
+  app.use((err, req, res, next) => {
+    console.error(err)
+    res.json({
+      success: false,
+      error: err.message
+    })
+  })
 
 } //end module.exports
