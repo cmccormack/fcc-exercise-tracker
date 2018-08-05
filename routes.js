67534c27941ef8a5b3bb473afb161b9226f5bad5
Christopher McCormack <mccormack.christopher@gmail.com>
2018-08-05 08:43:14 -0700

    Added validation for username and exercise, modified endpoint for exercise

--- routes.js
+++ routes.js
@@ -1,13 +1,15 @@
 const path = require('path')
-const User = require('./userModel')
+const { body, validationResult, } = require('express-validator/check')
+const { sanitizeBody, } = require('express-validator/filter')
 
+const User = require('./userModel')
 const root = path.resolve(__dirname, ".")
 const publicPath = path.join(root, "public")
 
 module.exports = function(app) {
 
   // DEBUG use to delete users as needed, delete later
-  // User.deleteOne({ _id: "5b64c412b8bef220dc4b4c0d"}, (err, user) => {
+  // User.deleteOne({ _id: "5b6710b887e06925b22a8bcb"}, (err, user) => {
   //   if (err) return next(new Error(err))
   //   console.log(user)
   // })
@@ -47,11 +49,29 @@ module.exports = function(app) {
       })
   })
 
-  //Route for creating user
-  app.post('/api/new-user', (req, res, next) => {
+  ///////////////////////////////////////////////////////////
+  // Register New User
+  ///////////////////////////////////////////////////////////
+  app.post('/api/new-user', [
+
+    // Username validation
+    body('username')
+      .trim()
+      .isLength({ min: 3, max: 20 })
+      .withMessage('Username must be between 3 and 20 characters, inclusive')
+      .isAlphanumeric()
+      .withMessage('Username must consist of only alphanumeric characters'),
+
+  ], (req, res, next) => {
     const { username } = req.body
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      const { param, msg: message, } = errors.array()[0]
+      return next({ param, message, })
+    }
+
     const newUser = new User({ username })
-    // Need to add username validation
 
     newUser.save(err => {
       if (err) {
@@ -68,7 +88,44 @@ module.exports = function(app) {
   })
 
   //Route for submitting exercise
-  app.post('/api/add-exercise', /* Add validation middleware */ (req, res, next) => {
+  app.post('/api/exercise/add', [
+
+    // Exercise validation
+    body('username')
+      .trim()
+      .isLength({ min: 3, max: 20 }).withMessage('Invalid Username')
+      .isAlphanumeric().withMessage('Invalid Username'),
+
+    body('description')
+      .trim()
+      .isLength({ min: 3, max: 50 })
+      .withMessage('Description must be between 3 and 20 characters, inclusive')
+      .optional({ checkFalsy: true, }).isAscii()
+      .withMessage('Description must contain only valid ascii characters'),
+
+    body('duration')
+      .trim()
+      .isLength({ min: 1, max: 9999 })
+      .withMessage('Duration must be between 1 and 9999 characters, inclusive')
+      .isNumeric()
+      .withMessage('Duration must be a numeric value'),
+
+    body('date')
+      .trim()
+      .isISO8601()
+      .withMessage('Invalid date')
+      .isAfter(new Date(0).toJSON())
+      .isBefore(new Date('2999-12-31').toJSON())
+      .withMessage("Invalid Date"),
+
+  ], (req, res, next) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      const { param, msg: message, } = errors.array()[0]
+      return next({ param, message, })
+    }
+
     const { username, description, duration, date } = req.body
     const newExercise = {
       description : description,
