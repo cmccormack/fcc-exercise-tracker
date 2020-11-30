30ea70a5050e0a9652e30f355e1502c963252276
Christopher McCormack <mccormack.christopher@gmail.com>
2020-11-29 19:28:48 -0700

    updated packages

--- server.js
+++ server.js
@@ -14,7 +14,14 @@ require("dotenv").config({ path: path.resolve(__dirname, ".env"), })
 //  Configure and connect to MongoDB database
 ///////////////////////////////////////////////////////////
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+console.log(process.env.MONGO_URI)
+mongoose.connect(
+    process.env.MONGO_URI,
+    { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .catch(({ message }) => {
     console.error(`Unable to connect to the mongodb instance: ${message}`)
   }
